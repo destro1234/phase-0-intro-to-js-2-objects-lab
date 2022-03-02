16ec1e83a1775102a53af5e5d6996bdd62a26034
Elias <eliasventura23@gmail.com>
2022-03-02 17:54:32 -0500

    commit tree 4a214d4832237d3511246365aceb3e043b405b28

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name: "Elias",
+    streetAddress: "33 Highland Ave",
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+    
+}
